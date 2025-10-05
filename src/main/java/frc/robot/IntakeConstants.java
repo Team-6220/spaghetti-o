1f0cfeb17fd366d8c2304b1013acdab50f02b701
mason <mason101488@gmail.com>
2025-10-05 15:03:09 -0700

    restore changes i think
    restoration

---
+++ src/main/java/frc/robot/IntakeConstants.java
@@ -0,0 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public final class IntakeConstants {
+    public static final int INTAKEMOTOR_ID = 15;
+    public static final boolean INVERTED = false;
+
+    public static final double ejectSpeed = -.25;
+    public static final double intakeSpeed = .5;
+  
+    public static final double maxCurrent = 20;
+    public static final double currentLimit = 5;
+    public static final double maxCurrentTime = 1;
+}
